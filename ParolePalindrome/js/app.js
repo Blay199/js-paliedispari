e416ba5ddbf3dfa4a49a9132a045fc2562d07862
Blay199 <a.sorrentino03@gmail.com>
2022-07-31 21:02:36 +0200

    Non funziona da correggere

--- ParolePalindrome/js/app.js
+++ ParolePalindrome/js/app.js
@@ -5,14 +5,16 @@ function palindroma(){
     console.log (chars)
     const charsRev = chars.reverse()
     console.log (charsRev)
-    for (let i = 0; i < chars.length; i++) {
-        if (chars[i] === charsRev[i]) {
-            console.log ('Parola Palindroma')
-        }else {
-            console.log ('Parola non Palindroma')
-        }
-    }
-    return word
+    const wordRev = charsRev.join('')
+    console.log (wordRev)
+
+    return word === wordRev
 }
 
 palindroma ()
+
+if (palindroma === true) {
+    console.log ('Parola Palindroma')
+} else {
+    console.log ('Parola non Palindroma')
+}
